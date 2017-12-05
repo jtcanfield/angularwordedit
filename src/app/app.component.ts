1934f64bb5a846ac70fc6d3529dbd18125681f22
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-12-05 09:13:27 -0500

    Add observable testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private data: Observable<Array<number>>;
+  private values: Array<number> = [];
+  private anyErrors: boolean;
+  private finished: boolean;
+
+  constructor() {
+  }
+
+  hello_button_clicked() {
+      this.data = new Observable(observer => {
+          // setTimeout(() => {
+          //     observer.next(42);
+          // }, 1000);
+          //
+          // setTimeout(() => {
+          //     observer.next(43);
+          // }, 2000);
+          //
+          // setTimeout(() => {
+          //     observer.complete();
+          // }, 3000);
+          this.status = "Started";
+          observer.next("First Thing");
+          observer.next("Second Thing");
+          observer.complete();
+      });
+
+      let subscription = this.data.forEach(v => this.values.push(v))
+        .then(() => this.status = "Ended");
+  }
 }
